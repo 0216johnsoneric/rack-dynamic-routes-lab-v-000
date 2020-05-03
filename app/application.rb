a34e0148af1d434dd4df0594ab6adfd9c74c46a0
Eric Johnson <ericjohnson0216@icloud.com>
2020-05-03 18:08:43 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,47 +1,27 @@
 class Application
+  class Application
+
   @@items = []
-  # @@items = ["Apples","Carrots","Pears"]
-  # @@cart = []
-  #
-  # def call(env)
-  #   resp = Rack::Response.new
-  #   req = Rack::Request.new(env)
-  #
-  #   if req.path.match(/items/)
-  #     @@items.each do |item|
-  #       resp.write "#{item}\n"
-  #     end
-  #   elsif req.path.match(/search/)
-  #     search_term = req.params["q"]
-  #     resp.write handle_search(search_term)
-  #   elsif req.path.match(/cart/)
-  #     if @@cart.empty?
-  #       resp.write "Your cart is empty"
-  #     else
-  #       @@cart.each do |item|
-  #         resp.write "#{item}\n"
-  #       end
-  #     end
-  #   elsif req.path.match(/add/)
-  #     item_to_add = req.params["item"]
-  #     if @@items.include? item_to_add
-  #       @@cart << item_to_add
-  #       resp.write "added #{item_to_add}"
-  #     else
-  #       resp.write "We don't have that item!"
-  #     end
-  #   else
-  #     resp.write "Path Not Found"
-  #   end
-  #
-  #   resp.finish
-  # end
-  #
-  # def handle_search(search_term)
-  #   if @@items.include?(search_term)
-  #     return "#{search_term} is one of our items"
-  #   else
-  #     return "Couldn't find #{search_term}"
-  #   end
-  # end
+
+  def call(env)
+    req = Rack::Request.new(env)
+    resp = Rack::Response.new
+
+    if req.path.match(/items\/.+/)
+      item_name = req.path.split("/items/").last
+      item = @@items.detect { |i| i.name == item_name }
+      if item
+        resp.write item.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+    resp.finish
+  end
+end
 end
